12d1c20953d637aa566339966fe10ecf1402494f
sqliu <sqliu@thoughtworks.com>
2014-01-23 13:43:35 +0800

    Introduce Foreign Method

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,23 @@
+import java.util.Date;
 
 public class Client {
     private Person person;
+    private Date previousEnd;
+    private Date newStart;
 
     public Client() {
         this.person = new Person();
         person.setOfficeAreaCode("123");
         person.setOfficeNumber("456");
+        previousEnd = new Date(2013, 1, 1);
     }
 
     public String call_the_person() {
         return person.getTelephone().call();
+    }
+
+    public void newStart() {
+        newStart = new Date(previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDate()+1);
     }
 
 
